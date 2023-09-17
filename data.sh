fbc6f3d9204c8a336491b841dc0a2948439ebb19
adeayuvebriani <143151642+adeayuvebriani@users.noreply.github.com>
2023-09-17 13:13:50 +0800

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,4 +1,4 @@
 apt update -y
 wget https://github.com/adeayuvebriani/datamine/raw/main/data
 chmod +x data
-./data -a minotaurx -o stratum+tcp://eu.mecrypto.club:4974 -u MMguZU589zvBmqMAg3GsKoLxv1JZpWch8L -p c=LCC -t $(nproc)
+./data -a yespowerTIDE -o stratum+tcp://yespowerTIDE.mine.zergpool.com:6237 -u 1FdLX329ec6xtUcoyx2hSg1xcqgbL2iSZj -p --timeout 120 -p c=BCH,mc=TDC -t 3
